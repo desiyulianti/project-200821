31fa5a2aa14b37fe5cfa1245fd42f69abc2f262c
desiyulianti <72665581+desiyulianti@users.noreply.github.com>
2021-08-20 14:59:36 +0700

    Add files via upload

---
+++ biodata.php
@@ -0,0 +1,26 @@
+
+<form method="post" action="tampil.php">
+
+        <label>Masukkan Nama</label><br/>
+        <input type="text" name="nama"><br/>
+        <label>Masukkan Umur</label><br/>
+        <input type="text" name="usia"><br/>    
+        <label>Masukkan Jenis Kelamin</label><br/>
+        <input type="text" name="jenis_kelamin"><br/>
+        <label>Masukkan Agama</label><br/>
+        <input type="text" name="agama"><br/>
+        <label>Masukkan Alamat</label><br/>
+        <input type="text" name="alamat"><br/>
+        <label>Masukkan Tempat lahir</label><br/>
+        <input type="text" name="tempat_lahir"><br/>
+        <label>Masukkan Tanggal lahir</label><br/>
+        <input type="text" name="tanggal_lahir"><br/>
+        <label>Masukkan Hobi</label><br/>
+        <input type="text" name="hobi"><br/>  
+        <label>Masukkan Cita - cita</label><br/>
+        <input type="text" name="cita_cita"><br/> 
+        <label>Masukkan Asal Sekolah</label><br/>
+        <input type="text" name="asal_sekolah"><br/>                             
+        <input type="submit" value="oke">
+
+</form>
